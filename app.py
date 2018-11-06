8a9a466cf43cafda3f6b0996c4e42d60171123f8
Minsu Kang <kms920612@gmail.com>
2018-11-06 06:47:30 +0000

    add post transport

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -15,23 +15,47 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    posts = Post.query.all()    # SELECT * FROM posts;
+    # posts = Post.query.all()    # SELECT * FROM posts;
+    posts = Post.query.order_by(Post.id.desc()).all()   # SELECT * FROM posts ORDER BY id DESC;
     return render_template('index.html', posts=posts)
 
 @app.route('/posts/new')
 def new():
     return render_template('new.html')
     
-@app.route('/posts/create')
+@app.route('/posts/create', methods=['POST'])
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
+    # title = request.args.get('title')     # get 방식에서 request받기
+    # content = request.args.get('content')
+    title = request.form.get('title')       # post방식에서 request받기
+    content = request.form.get('content')
     post = Post(title=title, content=content)
     db.session.add(post)
     db.session.commit()
-    return render_template('create.html', post=post)
+    return redirect('/posts/{}'.format(post.id))
     
 @app.route('/posts/<int:id>')
 def read(id):
     post = Post.query.get(id)   # SELECT * FROM posts WHERE id=1;
     return render_template('read.html', post=post)
+    
+@app.route('/posts/<int:id>/delete')
+def delete(id):
+    post = Post.query.get(id)   # DELETE FROM posts WHERE id=#;
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+    
+@app.route('/posts/<int:id>/edit')
+def edit(id):
+    post = Post.query.get(id)
+    return render_template('edit.html', post=post)
+    
+@app.route('/posts/<int:id>/update', methods=['POST'])
+def update(id):
+    post = Post.query.get(id)
+    post.title = request.form.get('title')
+    post.content = request.form.get('content')
+    db.session.commit()
+    return redirect('/posts/{}'.format(id))
+    
