7d147e2afd63ade6fd627857a1ee431668725d38
Minsu Kang <kms920612@gmail.com>
2018-11-07 00:45:17 +0000

    배포 준비 끝

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import *
-
+import os
 
 app = Flask(__name__)
 
 # DB 설정
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///board'   # 초기에 DB create 할때 지정한 db이름 board
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 db.init_app(app)
 migrate = Migrate(app, db)
